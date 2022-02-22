8b146e214eadd5c7a13f87c9bdc86e0599a7abfb
Caleb Mueller <mc61@hawaii.edu>
2022-02-21 17:23:59 -1000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-int main() {
+int main()
+{
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
    std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
    std::cout << std::endl;
-   std::cout << "My name is:  <<YOUR NAME HERE>>" << std::endl;
-   std::cout << "A fun fact about me is <<TELL ME HOW MUCH YOU LOVE CATS>>" << std::endl;
+   std::cout << "My name is:  Caleb Mueller" << std::endl;
+   std::cout << "A fun fact about me is I only have one nipple" << std::endl;
 
    return 0;
 }
